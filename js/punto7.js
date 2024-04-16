d00a9d2f84962708ecb9d5f1c3b61570f722919a
Tobias Zarlenga <tobiazarlenga@gmail.com>
2024-04-16 14:53:58 -0300

    teermino el punto 7 y tengo problemas con el git

---
+++ js/punto7.js
@@ -0,0 +1,19 @@
+let numrep = parseInt(
+  prompt("ingrese un numero de repeticiones entre 30 y 50  ")
+);
+if (!isNaN(numrep)) {
+  if (numrep >= 1 && numrep <= 50) {
+    for (let i = numrep; i <= 1; i--) {
+      for (let rep = i; rep >= 1; rep--) {
+        document.write(i);
+      }
+      document.write("<br>");
+    }
+
+    alert("el numero de repeticiones es " + numrep);
+  } else {
+    alert("el numero de repeticiones no es valido");
+  }
+} else {
+  alert("valor errooneo");
+}
